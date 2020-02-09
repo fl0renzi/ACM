fb737724638101680bb76f694db19313c6dc5b0c
Johan <elvemoon@gmail.com>
2020-02-09 12:24:36 +0100

    COUPLING

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACM.BL
 {
@@ -11,11 +12,14 @@ namespace ACM.BL
         public Order(int orderId)
         {
             OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
+        public int CustomerID { get; set; }
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
-
+        public List<OrderItem> OrderItems { get; set; }
+        public int ShippingAddressId { get; set; }
 
         public bool Validate()
         {
